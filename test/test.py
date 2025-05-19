941fa82941cfaa699bd8a6d0b12675c004970c57
saikrishnakompelly-bilvantis <sai.kompellt@bilvantis.io>
2025-05-19 14:18:53 +0530

    sample test

--- test/test.py
+++ test/test.py
@@ -67,10 +67,3 @@ class SecretManager:
         return {
             "access_key": "AKIAIOSFODNN7EXAMPLE",
             "secret_key": "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY",
-        }
-         def get_aws_credentials(self):
-        """Return AWS credentials."""
-        return {
-            "access_key": "AKIAIOSFODNN7EXAMPLE",
-            "secret_key": "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY",
-        }
